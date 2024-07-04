48348cd175b856df82cee0874664f3f052d9c5cc
hansmohob <36814586+hansmohob@users.noreply.github.com>
2024-07-04 16:57:12 +0100

    gone

--- frontend/src/src/pages/configurable-dashboard/interfaces.ts
+++
@@ -1,8 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-export interface StoredWidgetPlacement {
-  id: string;
-  columnOffset?: Record<number, number>;
-  rowSpan?: number;
-  columnSpan?: number;
-}
--- frontend/src/src/pages/dashboard/components/density-preferences/images/index.ts
+++
@@ -1,4 +0,0 @@
-// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-// SPDX-License-Identifier: MIT-0
-export { default as comfortableModeImage } from './comfortable-visual-refresh';
-export { default as compactModeImage } from './compact-visual-refresh';
