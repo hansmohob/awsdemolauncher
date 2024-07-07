322d4bbbb3fdccddb82801e3fc0820d817eb6c4c
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2024-07-07 16:17:26 +0100

    Update index.js

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,18 +1,42 @@
-import React from 'react';
-import SideNavigation from '@cloudscape-design/components/side-navigation';
+import * as React from "react";
+import SideNavigation from "@cloudscape-design/components/side-navigation";
 
-const items = [
-  { type: 'link', text: 'Demos', href: '/home/index.html' },
-];
-
-export default function Navigation() {
+export default () => {
+  const [activeHref, setActiveHref] = React.useState("#/page1");
   return (
-    <>
-      <SideNavigation
-        activeHref={window.location.pathname}
-        header={{ href: '/home/index.html', text: 'Service' }}
-        items={items}
-      />
-    </>
+    <SideNavigation
+      activeHref={activeHref}
+      header={{ href: "#/", text: "AWS Demo Launcher" }}
+      onFollow={(event) => {
+        if (!event.detail.external) {
+          event.preventDefault();
+          setActiveHref(event.detail.href);
+        }
+      }}
+      items={[
+        { type: "link", text: "Demos", href: "#/page1" },
+        { type: "link", text: "Deployed", href: "#/page2" },
+        { type: "link", text: "Connect", href: "#/page3" },
+        { type: "link", text: "Destroy", href: "#/page4" },
+        { type: "divider" },
+        {
+          type: "link",
+          text: "Notifications",
+          href: "#/notifications",
+        },
+        {
+          type: "link",
+          text: "Documentation",
+          href: "https://example.com",
+          external: true,
+        },
+        {
+          type: "link",
+          text: "GitHub",
+          href: "https://example.com",
+          external: true,
+        },
+      ]}
+    />
   );
-}
+};
