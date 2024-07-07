12247ba66f80500af6417b823a1d2c7f34fbc248
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2024-07-07 16:16:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import TopNavigation from '@cloudscape-design/components/top-navigation';
-import AppLayout from '@cloudscape-design/components/app-layout';
-import Navigation from './components/navigation';
-import Breadcrumbs from './components/breadcrumbs';
-import './styles.css';
+import React from "react";
+import TopNavigation from "@cloudscape-design/components/top-navigation";
+import AppLayout from "@cloudscape-design/components/app-layout";
+import Navigation from "./components/navigation";
+import Breadcrumbs from "./components/breadcrumbs";
+import CardView from "./components/cards"; // Import CardView component
+
+import "./styles.css";
 
 export default function App() {
   return (
@@ -11,29 +13,30 @@ export default function App() {
       <div id="top-nav">
         <TopNavigation
           identity={{
-            logo: { src: '/rocket.png', alt: 'Rocket' },
-            title: 'Demo Launcher',
-            href: '/home/index.html',
+            logo: { src: "/rocket.png", alt: "AWS Demo Launcher" },
+            title: "AWS Demo Launcher",
+            href: "/home/index.html",
           }}
           i18nStrings={{
-            overflowMenuTriggerText: 'More',
-            overflowMenuTitleText: 'All',
+            overflowMenuTriggerText: "More",
+            overflowMenuTitleText: "All",
           }}
         />
       </div>
       <AppLayout
         headerSelector="#top-nav"
         ariaLabels={{
-          navigation: 'Navigation drawer',
-          navigationClose: 'Close navigation drawer',
-          navigationToggle: 'Open navigation drawer',
-          notifications: 'Notifications',
-          tools: 'Help panel',
-          toolsClose: 'Close help panel',
-          toolsToggle: 'Open help panel',
+          navigation: "Navigation drawer",
+          navigationClose: "Close navigation drawer",
+          navigationToggle: "Open navigation drawer",
+          notifications: "Notifications",
+          tools: "Help panel",
+          toolsClose: "Close help panel",
+          toolsToggle: "Open help panel",
         }}
         breadcrumbs={<Breadcrumbs />}
         navigation={<Navigation />}
+        content={<CardView />} // Add CardView to the AppLayout content
       />
     </>
   );
