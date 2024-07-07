fbfd9bac0e2854002f6f3d1962e05c3e46a37d81
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2024-07-07 13:52:53 +0100

    Update index.js

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -1,40 +1,17 @@
 import React from 'react';
-import TopNavigation from '@cloudscape-design/components/top-navigation';
-import AppLayout from '@cloudscape-design/components/app-layout';
-import Navigation from './components/navigation';
-import Breadcrumbs from './components/breadcrumbs';
+import SideNavigation from '@cloudscape-design/components/side-navigation';
 
-import './styles.css';
+const items = [
+  { type: 'link', text: 'Demos', href: '/home/index.html' },
+];
 
-export default function App() {
+export default function Navigation() {
   return (
     <>
-      <div id="top-nav">
-        <TopNavigation
-          identity={{
-            logo: { src: '/rocket.png', alt: 'Rocket' },
-            title: 'Demo Launcher',
-            href: '/home/index.html',
-          }}
-          i18nStrings={{
-            overflowMenuTriggerText: 'More',
-            overflowMenuTitleText: 'All',
-          }}
-        />
-      </div>
-      <AppLayout
-        headerSelector="#top-nav"
-        ariaLabels={{
-          navigation: 'Navigation drawer',
-          navigationClose: 'Close navigation drawer',
-          navigationToggle: 'Open navigation drawer',
-          notifications: 'Notifications',
-          tools: 'Help panel',
-          toolsClose: 'Close help panel',
-          toolsToggle: 'Open help panel',
-        }}
-        breadcrumbs={<Breadcrumbs />}
-        navigation={<Navigation />}
+      <SideNavigation
+        activeHref={window.location.pathname}
+        header={{ href: '/home/index.html', text: 'Service' }}
+        items={items}
       />
     </>
   );
