b2c2d3c40ccadf28d6b75547724a1539c5e94113
Mo Mohoboob <36814586+hansmohob@users.noreply.github.com>
2024-07-09 07:16:10 +0100

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Initialize Terraform
+cd /mnt/efs/terraform/tfdeployed
+terraform init -input=false \
+    -backend-config="bucket=${TF_STATE_BUCKET_NAME}" \
+    -backend-config="key=${TF_STATE_BUCKET_KEY}" \
+    -backend-config="region=${AWS_REGION}" \
+    -backend-config="encrypt=true" \
+    -backend-config="dynamodb_table=${TF_STATE_DYNAMODB_TABLE}"
+
+# Start Node.js server
+cd /app
+exec node server.js
